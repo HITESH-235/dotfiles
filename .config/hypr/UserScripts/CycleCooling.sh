924eabaf1b345888e0e62fec9c8ca487adab887a
Hitesh Sinha <hiteshsinha2007@gmail.com>
2026-07-17 03:04:23 +0530

    Update CycleCooling.sh to use dynamic system icons for power profiles

--- .config/hypr/UserScripts/CycleCooling.sh
+++ .config/hypr/UserScripts/CycleCooling.sh
@@ -18,8 +18,17 @@ powerprofilesctl set "$next"
 # Get the new active asusctl profile for notification
 profile=$(asusctl profile get | grep "Active profile:" | awk '{print $NF}')
 
+# Select appropriate system icon based on the profile
+if [ "$profile" = "Performance" ]; then
+    icon="power-profile-performance-symbolic"
+elif [ "$profile" = "Quiet" ]; then
+    icon="power-profile-power-saver-symbolic"
+else
+    icon="power-profile-balanced-symbolic"
+fi
+
 # Send a notification (OSD)
-notify-send -e -u low -i "fan" "Cooling Mode" "Set to: $profile"
+notify-send -e -u low -i "$icon" "Cooling Mode" "Set to: $profile"
 
 # Refresh waybar
 pkill -RTMIN+10 waybar
